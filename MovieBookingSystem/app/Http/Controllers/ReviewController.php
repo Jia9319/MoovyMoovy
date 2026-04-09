63c3ff9802ab272358df2f7c028e58f7244043fc
xuen <tiuhanxuen@gmail.com>
2026-04-09 08:36:55 +0800

    web.php changes

--- MovieBookingSystem/app/Http/Controllers/ReviewController.php
+++ MovieBookingSystem/app/Http/Controllers/ReviewController.php
@@ -9,10 +9,7 @@ use Illuminate\Support\Facades\Auth;
 
 class ReviewController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     public function store(Request $request, Movie $movie)
     {
